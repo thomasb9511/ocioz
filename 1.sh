81b6190e4f0473c74cc3577333cde54bbadb569b
thomasb9511 <thomasb9511@users.noreply.github.com>
2025-12-27 17:48:02 -0500

    Add OpenColorIO installation and configuration commands

---
+++ 1.sh
@@ -0,0 +1,6 @@
+pip install opencolorio
+ociocpuinfo
+git clone https://github.com/analogstudio/OpenColorIO-Config-ACES-releases
+ocioarchive reference-config-v1.0.0_aces-v1.3_ocio-v2.1 --iconfig "./OpenColorIO-Config-ACES-releases/v1.0.0/reference-config-v1.0.0_aces-v1.3_ocio-v2.1.ocio"
+ocioarchive reference-config-v3.0.0_aces-v2.0_ocio-v2.4 --iconfig "./OpenColorIO-Config-ACES-releases/v3.0.0/reference-config-v3.0.0_aces-v2.0_ocio-v2.4.ocio"
+ocioarchive reference-config-v4.0.0_aces-v2.0_ocio-v2.5 --iconfig "./OpenColorIO-Config-ACES-releases/v4.0.0/reference-config-v4.0.0_aces-v2.0_ocio-v2.5.ocio"
